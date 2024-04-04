efae870caa325774f96d879e3eca55963d19eec6
Jesse Treleaven <jessetreleaven@gmail.com>
2024-04-03 21:55:00 -0400

    Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CardCounterOmega2 from './CardCounterOmega2';
+
+function App() {
+  return (
+    <div className="App">
+      <CardCounterOmega2 />
+    </div>
+  );
+}
+
+export default App;
